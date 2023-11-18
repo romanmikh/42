f89b8554124dfc6a59ebbcf6a0767610d622d8bb
Roman Mikhaylenko <roman.mikhaylenko14@hotmail.com>
2023-11-18 18:28:56 +0000

    Extra functions

---
+++ ft_stock_str.h
@@ -0,0 +1,21 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_stock_str.h                                     :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: rmikhayl <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/10/25 22:04:59 by rmikhayl          #+#    #+#             */
+/*   Updated: 2023/10/25 22:07:06 by rmikhayl         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef FT_STOCK_STR_H
+# define FT_STOCK_STR_H
+typedef struct s_stock_str
+{
+	int size;
+	char *str;
+	char *copy;
+}	t_stock_str;
+#endif
---
+++ ft_str_to_tab.c
@@ -0,0 +1,26 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_str_to_tab.c                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: rmikhayl <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/10/25 22:03:56 by rmikhayl          #+#    #+#             */
+/*   Updated: 2023/10/25 22:09:55 by rmikhayl         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <stdio.h>
+#include <unistd.h>
+#include "ft_stock_str.h"
+
+struct s_stock_str *ft_strs_to_tab(int ac, char **av)
+{
+	
+}
+
+int	main(argc, **argv)
+{
+	.
+	ft_strs_to_tab(ac, av);
+}
