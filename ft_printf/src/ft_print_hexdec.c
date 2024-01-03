997e701249ffaa02bf3026298e3179461d9b050f
Roman Mikhaylenko <roman.mikhaylenko14@hotmail.com>
2024-01-03 20:41:50 +0000

    Improving helper functions

---
+++ ft_printf/src/ft_print_hexdec.c
@@ -0,0 +1,47 @@
+# include "ft_printf.h"
+
+int ft_hex_len(unsigned int n)
+{
+    int len;
+
+    len = 0;
+    while (n != 0)
+    {
+        n = n / 16;
+        len++;
+    }
+    return (len);
+}
+
+void	ft_put_hex(unsigned int n, const char upper_lower)
+{
+	if (n >= 16)
+	{
+		ft_put_hex(n / 16, upper_lower);
+		ft_put_hex(n % 16, upper_lower);
+	}
+	else
+	{
+		if (n <= 9)
+			ft_putchar_fd((n + '0'), 1);
+		else
+		{
+			if (upper_lower == 'lower')
+				ft_putchar_fd((n - 10 + 'a'), 1);
+			if (upper_lower == 'upper')
+				ft_putchar_fd((n - 10 + 'A'), 1);
+		}
+	}
+}
+
+int	ft_print_hex(unsigned int n, const char upper_lower)
+{
+	if (n == 0)
+    {
+        ft_putchar_fd("0", 1);
+		return (1);
+    }
+	else
+		ft_put_hex(n, upper_lower);
+	return (ft_hex_len(n));
+}
