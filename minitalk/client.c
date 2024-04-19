3ed2c011fc531b399009bfd88bc50785959ec377
¨Roman <roman.mikhaylenko14@hotmail.com>
2024-04-19 14:18:53 +0100

    minitalk corrected for norminette

--- minitalk/client.c
+++ minitalk/client.c
@@ -6,51 +6,47 @@
 /*   By: rocky <marvin@42.fr>                       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/16 19:42:10 by rocky             #+#    #+#             */
-/*   Updated: 2024/04/16 19:42:11 by rocky            ###   ########.fr       */
+/*   Updated: 2024/04/19 14:06:51 by rmikhayl         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-# include "libft/libft.h"
+#include "libft/libft.h"
 
-void send_signal(int PID, unsigned char character)
+void	send_signal(int process_id, unsigned char character)
 {
-  // convert character into 8 bits, send to server
-  // SIGUSR1 represents a 1 bit, SIGUSR2 to represent a 0 bit.
-  int i;
-  unsigned char temp_char;
+	int				i;
+	unsigned char	temp_char;
 
-  i = 8;
-  temp_char = character;
-  while (i > 0)
-  {
-    i--;
-    temp_char = character >> i;
-    if (temp_char % 2 == 0)
-      kill(PID, SIGUSR2);
-    else 
-      kill(PID, SIGUSR1);
-    usleep(69);
-  }
-  
+	i = 8;
+	temp_char = character;
+	while (i-- > 0)
+	{
+		temp_char = character >> i;
+		if (temp_char % 2 == 0)
+			kill(process_id, SIGUSR2);
+		else
+			kill(process_id, SIGUSR1);
+		usleep(300);
+	}
 }
 
-int main(int argc, char *argv[])
+int	main(int argc, char *argv[])
 {
-  int i;
-  pid_t PID;
-  char *str;
+	int			i;
+	int			process_id;
+	const char	*str;
 
-
-  if (argc != 3)
-  {
-    ft_printf("Please provide two arguments: PID & the string to send.");
-    return (1);
-  }
-  PID = ft_atoi(argv[1]);
-  str = argv[2];
-  i = 0;
-  while (str[i] != '\0')
-    send_signal(PID, str[i]);
-  send_signal(PID, '\0');
-  return (0);
+	if (argc != 3)
+	{
+		ft_printf("Please provide two arguments: process_id & the \
+				string to send.");
+		return (1);
+	}
+	process_id = ft_atoi(argv[1]);
+	str = argv[2];
+	i = 0;
+	while (str[i] != '\0')
+		send_signal(process_id, str[i++]);
+	send_signal(process_id, '\0');
+	return (0);
 }
--- minitalk/server.c
+++ minitalk/server.c
@@ -3,41 +3,39 @@
 /*                                                        :::      ::::::::   */
 /*   server.c                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: rocky <marvin@42.fr>                       +#+  +:+       +#+        */
+/*   By: rmikhayl <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/04/16 19:42:15 by rocky             #+#    #+#             */
-/*   Updated: 2024/04/16 19:42:17 by rocky            ###   ########.fr       */
+/*   Created: 2024/04/19 14:17:52 by rmikhayl          #+#    #+#             */
+/*   Updated: 2024/04/19 14:18:09 by rmikhayl         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-# include "libft/libft.h"
-
-void handle_signal(int signal)
+void	handle_signal(int signal)
 {
-  static unsigned char current_char;
-  static int bit_index;
+	static unsigned char	current_char;
+	static int				bit_index;
 
-  current_char |= (signal == SIGUSR1);
-  bit_index++;
-  if (bit_index == 8){
-    if (current_char == '\0')
-      ft_printf("\n");
-    else
-      ft_printf("%c", current_char);
-    bit_index = 0;
-    current_char = 0;
-  }
-  else 
-    current_char <<= 1;
+	current_char |= (signal == SIGUSR1);
+	bit_index++;
+	if (bit_index == 8)
+	{
+		if (current_char == '\0')
+			ft_printf("\n");
+		else
+			ft_printf("%c", current_char);
+		bit_index = 0;
+		current_char = 0;
+	}
+	else
+		current_char <<= 1;
 }
 
-int main()
+int	main(void)
 {
-  ft_printf("%d\n", getpid());
-  signal(SIGUSR1, handle_signal);
-  signal(SIGUSR2, handle_signal);
-  // listen for & receive string
-  while (1)
-    pause();
-  return (0);
+	ft_printf("%d\n", getpid());
+	signal(SIGUSR1, handle_signal);
+	signal(SIGUSR2, handle_signal);
+	while (1)
+		pause();
+	return (0);
 }
