789e0dda62604a273755b58c63b38fcf60f60ab7
¨Roman <roman.mikhaylenko14@hotmail.com>
2024-04-23 16:21:05 +0100

    Fixed minitalk space -> @ error, passed norminette

--- minitalk/client.c
+++ minitalk/client.c
@@ -6,27 +6,28 @@
 /*   By: rocky <marvin@42.fr>                       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/16 19:42:10 by rocky             #+#    #+#             */
-/*   Updated: 2024/04/19 14:30:00 by rmikhayl         ###   ########.fr       */
+/*   Updated: 2024/04/23 15:22:16 by rmikhayl         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft/libft.h"
 
+// * Loop from MSB to LSB
+// * Check ith bit of character with bitwise bitmask &, if 1 (true), SIGUSR1
+// * Sleep to avoid signal overlap   
+
 void	send_signal(int process_id, unsigned char character)
 {
-	int				i;
-	unsigned char	temp_char;
+	int	i;
 
 	i = 8;
-	temp_char = character;
-	while (i-- > 0)
+	while (--i >= 0)
 	{
-		temp_char = character >> i;
-		if (temp_char % 2 == 0)
-			kill(process_id, SIGUSR2);
-		else
+		if ((character & (1 << i)) != 0)
 			kill(process_id, SIGUSR1);
-		usleep(1000);
+		else
+			kill(process_id, SIGUSR2);
+		usleep(500);
 	}
 }
 
@@ -38,8 +39,7 @@ int	main(int argc, char *argv[])
 
 	if (argc != 3)
 	{
-		ft_printf("Please provide two arguments: process_id & the \
-				string to send.");
+		ft_printf("Please provide two arguments: process_id & string.");
 		return (1);
 	}
 	process_id = ft_atoi(argv[1]);
--- minitalk/server.c
+++ minitalk/server.c
@@ -6,29 +6,41 @@
 /*   By: rmikhayl <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/19 14:17:52 by rmikhayl          #+#    #+#             */
-/*   Updated: 2024/04/19 17:11:32 by rmikhayl         ###   ########.fr       */
+/*   Updated: 2024/04/23 16:18:37 by rmikhayl         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft/libft.h"
 
+// Reconstruct characters from incoming bits. SIGUSR1 ->  binary 1, SIGUSR2 -> 0
+// bit_count & current_char both stored in static 'data' via 8bit shift
+// int 'data' 32-bit by default, we utilise MS8B for bit_count, LS8B for curr_ch
+// 00000000 00000000 bbbbbbbb cccccccc <- format of 'data' after repackaging
+// Prints characters upon reconstructing a full byte
+// Resets and continues for next character after each byte
+
 void	handle_signal(int signal)
 {
-	static unsigned char	current_char;
-  unsigned char temp_char;
+	static int		data = 0;
+	int				bit_count;
+	unsigned char	current_char;
 
-  temp_char = current_char;
-	current_char |= (signal == SIGUSR1);
-	if (ft_count_bits(temp_char) == 7)
+	bit_count = data >> 8;
+	current_char = (unsigned char)data;
+	current_char <<= 1;
+	if (signal == SIGUSR1)
+		current_char |= 1;
+	bit_count++;
+	if (bit_count == 8)
 	{
 		if (current_char == '\0')
 			ft_printf("\n");
 		else
 			ft_printf("%c", current_char);
 		current_char = 0;
+		bit_count = 0;
 	}
-	else
-		current_char <<= 1;
+	data = (bit_count << 8) | current_char;
 }
 
 int	main(void)
