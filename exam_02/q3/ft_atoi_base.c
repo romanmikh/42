7fda50bc391748a3fda396d6b0f21e2c5ebdabe9
Roman Mikhaylenko <roman.mikhaylenko14@hotmail.com>
2024-05-02 19:30:05 +0100

    Auxiliary functions

--- exam_02/q3/ft_atoi_base.c
+++ exam_02/q3/ft_atoi_base.c
@@ -1,9 +1,59 @@
-int ft_atoi_base(const char *str, int str_base)
-{
-  
+#include <stdio.h>
+#include <stdlib.h>
+
+int power(int a, int b){
+  int i = 0;
+  int pow = 1;
+
+  while (i < b){
+    pow *= a;
+    i++;
+  }
+  printf("pow: %d\n", pow);
+  return (pow);
 }
 
-int main()
+int convert_digit_to_b10(char c){
+  int i = 0;
+  char *max_base = "0123456789abcdef";
+
+  while (i < 16){
+    if (max_base[i] == c)
+      return (i);
+    i++;
+  }
+  return (0);
+}
+
+int ft_strlen(const char *str){
+  int i = 0;
+
+  while (str[i])
+    i++;
+  return (i-1);
+}
+
+int ft_atoi_base(const char *str, int str_base)
 {
+  int i = 0;
+  int b10 = 0;
+  int digit = ft_strlen(str);
+
+  if (str_base == 10)
+    return (atoi(str));
+
+      while (i <= ft_strlen(str)){
+      b10 += convert_digit_to_b10(str[i]) * power(str_base, digit);
+      i++;
+      digit--;
+    }
+  return (b10);
+}
+
+int main(int argc, char **argv)
+{
+  char *b3 = "0201";
+  char *b16 = "12fdb3";
+  printf("%s in base %d: %d\n", argv[1], atoi(argv[2]), ft_atoi_base(argv[1], atoi(argv[2])));
   return (0);
 }
---
+++ exam_02/q3/ft_list_size.c
@@ -0,0 +1,15 @@
+#include "ft_list.h"
+
+int ft_list_size(t_list *begin_list)
+
+  
+int main()
+{
+  t_list list;
+  list = (t_list *)malloc(sizeof(t_list));
+  list->data = 1;
+  list->next = 
+  
+  printf("list len: %d", ft_list_size(list));
+  return (0);
+}
