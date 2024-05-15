118770859387c70994ad84e19341fa3809adce09
¨Roman <roman.mikhaylenko14@hotmail.com>
2024-05-15 15:07:07 +0100

    dev: fractol

--- final_fractol/srcs/main.c
+++ final_fractol/srcs/main.c
@@ -63,7 +63,12 @@ int	main(int ac, char **av)
 	t_god		god;
 
 	if (ac == 1)
+	{
+		ft_printf("\033[1;31mPlease execute as follows:\033[0m \
+		./exec M/J/MB/BS\n");
 		return (0);
+		}
+		
 	if (handle_args(&god, ac, av) == FALSE)
 		return (2);
 	init_god(&god);
